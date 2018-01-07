c1b3834fed84eea2ed0903c98fc149c66ac77811
klausbachmann <martin@derwolters.de>
2018-01-07 21:30:52 +0100

    .

---
+++ Assets/GameController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject circle;
+    public float gameSpeed = 0.5f;
+    bool waiting = false;
+    public int circlesHitted;
+    public int circlesMissed;
+
+    public Text txtHits, txtMissed, txtAccuracy;
+    
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    public void hit() {
+        circlesHitted++;
+        txtHits.text = "Hits: " +  circlesHitted.ToString();
+    }
+
+    public void miss() {
+        circlesMissed++;
+        txtMissed.text = "Missed: " + circlesHitted.ToString();    
+    }
+
+    public IEnumerator wait()
+    {
+        waiting = true;
+        yield return new WaitForSeconds(gameSpeed);
+        waiting = false;
+        print("CREATE");
+        
+
+        GameObject p;
+        p = Instantiate(circle);
+       
+        p.transform.position = new Vector2(Random.Range(-8, 8), Random.Range(-4, 4));
+        p.GetComponent<Animator>().Play("circleAnimation");
+
+    }
+
+    void FixedUpdate(){
+        if (!waiting) {
+            StartCoroutine(wait());
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/circleController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class circleController : MonoBehaviour {
+
+
+    public GameController gameController;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+
+    // Update is called once per frame
+	void Update () {
+        
+	}
+
+ 
+
+    void destroyObject() {
+        gameController.miss();
+        Destroy(gameObject);
+    }
+
+    void OnMouseDown() {
+        gameController.hit();
+        Destroy(gameObject);
+    }
+}
